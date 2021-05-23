1f8b362be5a858ce251c887490a66043b726de76
Julius Ijie <juliusijie@gmail.com>
2021-05-23 23:43:07 +0100

    Linting changes

--- src/config/router.ts
+++ src/config/router.ts
@@ -3,7 +3,7 @@ import primeRouter from "../controllers/prime";
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
 	res.json({
 		status: true,
 		message: "Hello world",
