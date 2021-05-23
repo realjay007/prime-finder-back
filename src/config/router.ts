7494480fa85acb32020459db65b7ea5654c57d22
Julius Ijie <juliusijie@gmail.com>
2021-05-23 23:05:13 +0100

    Add prime controller to router

--- src/config/router.ts
+++ src/config/router.ts
@@ -1,6 +1,15 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import primeRouter from "../controllers/prime";
 
 const router = Router();
 
+router.get('/', (req: Request, res: Response) => {
+	res.json({
+		status: true,
+		message: "Hello world",
+	});
+});
+
+router.use("/prime", primeRouter);
 
 export default router;
